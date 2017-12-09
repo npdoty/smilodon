fd994ea320f87111dd10b2846cb93c79b9a7700a
Nick Doty <npdoty@ischool.berkeley.edu>
2017-12-08 19:49:14 -0800

    npd-specific-config for heroku testing

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import urllib
 
 API_NAME = os.environ['api_name']
 API_URI = os.environ['api_uri']
@@ -7,10 +8,12 @@ SERVER_URI = os.environ['server_uri']
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = os.environ['secret_key']
-DB_NAME = 'Cluster0'
 
-MONGO_URI = 'mongodb://rowanlupton:'+os.environ['mongodb-password']+'@cluster0-shard-00-00-jjly0.mongodb.net:27017,cluster0-shard-00-01-jjly0.mongodb.net:27017,cluster0-shard-00-02-jjly0.mongodb.net:27017/smilodon?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+MONGO_USERNAME = 'npdoty'
+MONGO_PASSWORD = os.environ['mongodb_password']
+MONGO_URI = 'mongodb://'+urllib.parse.quote_plus(MONGO_USERNAME)+':'+urllib.parse.quote_plus(MONGO_PASSWORD)+'@cluster0-shard-00-00-edece.mongodb.net:27017,cluster0-shard-00-01-edece.mongodb.net:27017,cluster0-shard-00-02-edece.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 MONGO_DBNAME = 'Cluster0'
+DB_NAME = 'Cluster0'
 
 valid_header = 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
 CONTENT_HEADERS = {'Content-Type': valid_header}
@@ -31,4 +34,4 @@ MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 # administrator list
-ADMINS = ['rowanlupton@icloud.com']
+ADMINS = ['nick@npdoty.name']
